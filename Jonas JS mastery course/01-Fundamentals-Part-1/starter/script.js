b65edb186688f1d81b738cde3fde540931b8ec02
Ali Munir <alimunirwaris1@gmail.com>
2023-09-12 07:22:19 +0500

    Challenge 2 completed

--- Jonas JS mastery course/01-Fundamentals-Part-1/starter/script.js
+++ Jonas JS mastery course/01-Fundamentals-Part-1/starter/script.js
@@ -91,7 +91,48 @@ let heightJohn = 1.95;
 let BMIMark = massMark / (heightMark * heightMark);
 let BMIJohn = massJohn / (heightJohn * heightJohn);
 
-console.log(BMIMark);
-console.log(BMIJohn);
+let markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark);
+// console.log(BMIJohn);
+// console.log(markHigherBMI);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
+
+let job = "Software Engineer";
+
+let age = 20;
+
+let newDescription = `I am Alee from ${country}. I am ${age} years old.`;
+
+// console.log(newDescription);
+
+if (age >= 18) {
+  console.log(`Yeah this man is adult now`);
+} else {
+  console.log(`You are just a kid`);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #2
+// Use the BMI example from Challenge #1, and the code you already wrote, and
+// improve it.
+// Your tasks:
+// 1. Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!
+
+if (markHigherBMI) {
+  console.log(
+    `Mark's BMI (${BMIMark.toFixed(
+      2
+    )}) is higher than John's (${BMIJohn.toFixed(2)})`
+  );
+} else {
+  console.log(
+    `John's BMI (${BMIJohn.toFixed(
+      2
+    )}) is higher than Mark's (${BMIMark.toFixed(2)})`
+  );
+}
