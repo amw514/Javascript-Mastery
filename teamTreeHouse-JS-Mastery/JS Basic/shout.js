2c4cfdd633e28f7d32d1c181453737fe1cbe43df
Ali Munir <alimunirwaris1@gmail.com>
2023-09-15 04:05:05 +0500

    Completed General and basic JS

--- teamTreeHouse-JS-Mastery/JS Basic/shout.js
+++ teamTreeHouse-JS-Mastery/JS Basic/shout.js
@@ -2,6 +2,22 @@ let msg = "What do you wanna say?";
 
 let shout = "I love JavaScript";
 
-let shoutMessage = `${msg} : \n${shout}`;
+let shoutMessage = `<h1>${msg} : \n${shout}</h1>`;
 
 document.querySelector("main").innerHTML = shoutMessage;
+
+let correctGuess = false;
+
+let number = 6;
+
+let guess = prompt("Guess number: ");
+
+if (+guess === number) {
+  correctGuess = true;
+}
+
+if (correctGuess) {
+  console.log("Correct");
+} else {
+  console.log("false");
+}
