78272425685a17fdb58fb4598aacd062fc3205bb
Ali Munir <alimunirwaris1@gmail.com>
2023-09-18 00:03:17 +0500

    Add new function example

--- Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
+++ Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,27 @@
 "use strict";
 
+////////////////////////////////////////////////////////////////
+
 function add(a, b) {
   return a + b;
 }
 
 // console.log(add(4, 5));
 
+////////////////////////////////////////////////////////////////
+
 let subtract = (a, b) => {
   return a - b;
 };
 
 // console.log(subtract(10, 7));
+
+////////////////////////////////////////////////////////////////
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+console.log(fruitProcessor(5, 0));
