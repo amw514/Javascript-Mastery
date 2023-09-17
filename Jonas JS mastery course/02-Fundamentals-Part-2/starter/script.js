1583da266074a4946690a4f6d1e4c535fdd4c010
Ali Munir <alimunirwaris1@gmail.com>
2023-09-17 23:52:09 +0500

    Added Function and arrow function

--- Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
+++ Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
@@ -2,4 +2,10 @@ function add(a, b) {
   return a + b;
 }
 
-console.log(add(4, 5));
+// console.log(add(4, 5));
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+// console.log(subtract(10, 7));
