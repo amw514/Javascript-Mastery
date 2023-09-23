80a3cd8d2982e39ee71cde655cdeaa786f000fcd
Ali Munir <alimunirwaris1@gmail.com>
2023-09-23 19:50:44 +0500

    Completed fundamentals

--- Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
+++ Jonas JS mastery course/02-Fundamentals-Part-2/starter/script.js
@@ -79,6 +79,7 @@ let scoreKoalas = calcAverage(65, 54, 49);
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
     console.log("dolphins win");
+    P;
   } else if (avgKoalas >= 2 * avgDolphins) {
     console.log("koalas  win");
   } else {
@@ -95,3 +96,31 @@ checkWinner(scoreDolphins, scoreKoalas);
 const friends = ["Afriend", "Bfriend", "Cfriend", "Dfriend"];
 
 console.log(friends);
+
+// Challenge 4
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
