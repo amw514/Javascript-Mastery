8220d3b8c806e76abb31d575c35d84e91d621a39
Ali Munir <alimunirwaris1@gmail.com>
2023-09-26 02:20:40 +0500

    Updated Function example

--- teamTreeHouse-JS-Mastery/JS functions/index.js
+++ teamTreeHouse-JS-Mastery/JS functions/index.js
@@ -2,3 +2,13 @@ const getRandomNumber = () => {
   let randomNumber = Math.floor(Math.random() * 10) + 1;
   return randomNumber;
 };
+
+function sumEvenNumbers(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      sum += numbers[i];
+    }
+  }
+  return sum;
+}
