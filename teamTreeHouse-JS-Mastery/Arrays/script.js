86ca96d86b177aaeab0e50c5899dff147d502c92
Ali Munir <alimunirwaris1@gmail.com>
2023-09-27 21:14:44 +0500

    Updated Array

--- teamTreeHouse-JS-Mastery/Arrays/script.js
+++ teamTreeHouse-JS-Mastery/Arrays/script.js
@@ -1,3 +1,12 @@
 let newArray = ["a", "b", "c", "d", "e", "f"];
 
-let multidimensionalArr = [["a","b"],1,2]
+let multidimensionalArr = [["a", "b"], 1, 2];
+
+// Creating an array of numbers
+let numbers = [1, 2, 3, 4, 5];
+
+// Creating an array of strings
+let fruits = ["apple", "banana", "orange"];
+
+// Creating an array of mixed data types
+let mixed = [1, "apple", true, null, undefined];
