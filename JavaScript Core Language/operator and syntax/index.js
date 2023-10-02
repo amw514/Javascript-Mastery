c50c82f79cd6b3a7caeddf539e01c380acf22793
Ali Munir <alimunirwaris1@gmail.com>
2023-10-02 20:00:00 +0500

    Added New Switch Case example

--- JavaScript Core Language/operator and syntax/index.js
+++ JavaScript Core Language/operator and syntax/index.js
@@ -10,3 +10,22 @@ switch (day) {
   default:
     console.log("It's a regular day.");
 }
+
+// Another Switch Case example
+
+let fruit = "apple";
+
+switch (fruit) {
+  case "apple":
+  case "pear":
+  case "banana":
+    console.log("This is a fruit.");
+    break;
+  case "carrot":
+  case "broccoli":
+  case "celery":
+    console.log("This is a vegetable.");
+    break;
+  default:
+    console.log("I don't know what this is.");
+}
