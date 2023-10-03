011669bacbd6132e50bf0da38c79b4e19066eeb4
Ali Munir <alimunirwaris1@gmail.com>
2023-10-03 20:00:00 +0500

    Add new switch example

--- JavaScript Core Language/operator and syntax/index.js
+++ JavaScript Core Language/operator and syntax/index.js
@@ -29,3 +29,22 @@ switch (fruit) {
   default:
     console.log("I don't know what this is.");
 }
+
+// A detail view on switch
+
+let fruit = "apple";
+
+switch (fruit) {
+  case "apple":
+  case "pear":
+  case "banana":
+    console.log("This is a fruit.");
+    break;
+  case "carrot":
+  case "broccoli":
+  case "celery":
+    console.log("This is a vegetable.");
+    break;
+  default:
+    console.log("I don't know what this is.");
+}
