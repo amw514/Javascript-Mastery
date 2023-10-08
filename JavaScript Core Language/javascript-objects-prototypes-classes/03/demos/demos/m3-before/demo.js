a283c0029817fe115ff795cf62e27ad72e90bd85
Ali Munir <alimunirwaris1@gmail.com>
2023-10-08 17:07:35 +0500

    Object Properties

--- JavaScript Core Language/javascript-objects-prototypes-classes/03/demos/demos/m3-before/demo.js
+++ JavaScript Core Language/javascript-objects-prototypes-classes/03/demos/demos/m3-before/demo.js
@@ -1,12 +1,30 @@
-'use strict'; 
-(function() {
-
+"use strict";
+(function () {
   let person = {
-    name: {
-      first: 'Jim',
-      last: 'Cooper'
-    },
-    age: 29
+    first: "Jim",
+    last: "Cooper",
+    age: 29,
   };
 
+  person["hair color"] = "black";
+
+  display(person["first"]);
+
+  display(person["hair color"]);
+
+  let properName = "first";
+
+  display(person[properName]);
+
+  for (let key in person) {
+    display(key + ":" + person[key]);
+  }
+
+  Object.defineProperty(person, "first", { writable: false });
+
+  Object.defineProperty(person, "first", { enumerable: false });
+
+  Object.defineProperty(person, "first", { configurable: false });
+
+  display(Object.getOwnPropertyDescriptor(person, "first"));
 })();
