dfa48dac209a24634e99cb4ee01c22cb4f98a7d8
Ali Munir <alimunirwaris1@gmail.com>
2023-10-09 22:44:01 +0500

    Object Prototypes

--- JavaScript Core Language/javascript-objects-prototypes-classes/04/demos/demos/m4-before/demo.js
+++ JavaScript Core Language/javascript-objects-prototypes-classes/04/demos/demos/m4-before/demo.js
@@ -1,6 +1,27 @@
-'use strict'; 
-(function() {
+"use strict";
+(function () {
+  function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 
-  
+  // display(Person.prototype);
 
+  // display(jim.__proto__);
+
+  // display(Person.prototype === jim.__proto__);
+
+  Person.prototype.age = 29;
+
+  var jim = new Person("Jim", "Cooper");
+  let sofia = new Person("Sofia", "Cooper");
+
+  Person.prototype = { age: 18 };
+
+  let kris = new Person("Kris", "Cooper");
+
+  display(Person.prototype);
+  display(jim.age);
+  display(sofia.age);
+  display(kris.age);
 })();
