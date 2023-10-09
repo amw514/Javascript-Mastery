d37d82891e1c3b202cc1461e45194d72d82bd17b
Ali Munir <alimunirwaris1@gmail.com>
2023-10-10 04:40:45 +0500

    Classes Examples

--- JavaScript Core Language/javascript-objects-prototypes-classes/05/demos/demos/05-before/demo.js
+++ JavaScript Core Language/javascript-objects-prototypes-classes/05/demos/demos/05-before/demo.js
@@ -1,6 +1,42 @@
-'use strict'; 
-(function() {
+"use strict";
+(function () {
+  class Person {
+    constructor(firstName, lastName, age) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+    }
 
-  
+    get fullName() {
+      return this.firstName + " " + this.lastName;
+    }
 
+    set fullName(fullName) {
+      let nameParts = fullName.split(" ");
+      this.firstName = nameParts[0];
+      this.lastName = nameParts[1];
+    }
+
+    isAdult() {
+      return this.age >= 18;
+    }
+  }
+
+  let jim = new Person("Jim", "Cooper", 29);
+
+  jim.fullName = "Fred Jones";
+
+  display(jim);
+  display(jim.fullName);
+
+  class Student extends Person {
+    constructor(firstName, lastName, age) {
+      super(firstName, lastName, age);
+      this._enrolledCourses = [];
+    }
+
+    static fromPerson(person) {
+      return new Student(person.firstName, person.lastName, person.age);
+    }
+  }
 })();
