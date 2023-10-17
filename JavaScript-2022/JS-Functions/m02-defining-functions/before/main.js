885f0f067bf68815629ea13afe2c70992c081cbb
Ali Munir <alimunirwaris1@gmail.com>
2023-10-17 18:33:29 +0500

    Example of recursive function

--- JavaScript-2022/JS-Functions/m02-defining-functions/before/main.js
+++ JavaScript-2022/JS-Functions/m02-defining-functions/before/main.js
@@ -1,5 +1,13 @@
 // Create a new function that converts gallons
 // to liters: convertToLiters
 
+let recursiveFunction = (countingItems) => {
+  let answer = `${countingItems} items left`;
+  console.log(answer);
+  countingItems -= 100;
+  if (countingItems > 0) {
+    recursiveFunction(countingItems);
+  }
+};
 
-
+recursiveFunction(500);
