3f60f6b780e271e9ea3632a9c653c87828a9fcb0
Ali Munir <alimunirwaris1@gmail.com>
2023-10-17 20:28:26 +0500

    Getter Example

--- JavaScript-2022/JS-Functions/m02-defining-functions/before/main.js
+++ JavaScript-2022/JS-Functions/m02-defining-functions/before/main.js
@@ -4,10 +4,27 @@
 let recursiveFunction = (countingItems) => {
   let answer = `${countingItems} items left`;
   console.log(answer);
-  countingItems -= 100;
+  countingItems -= 1;
   if (countingItems > 0) {
     recursiveFunction(countingItems);
   }
 };
 
 recursiveFunction(500);
+
+// Getter
+
+class Passenger {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let john = new Passenger("John", "Doe");
+
+console.log(john.fullName);
